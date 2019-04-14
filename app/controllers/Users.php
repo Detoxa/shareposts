f3b8e802e076bca91d5573ff6c4a502f95f70ffe
Vit Safarik <vitsaf@gmail.com>
2019-04-14 16:32:47 +0200

    session data and logout

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -140,7 +140,7 @@ class Users extends Controller
 
                 if ($loggedInUser) {
                     // Create Session
-                    die('JSI PRIPOJENY');
+                    $this->createUserSession($loggedInUser);
                 } else {
                     $data['password_err'] = 'Password incorrect';
 
@@ -162,6 +162,32 @@ class Users extends Controller
 
             // Load view
             $this->view('users/login', $data);
+        }
+    }
+
+    public function createUserSession($user)
+    {
+        $_SESSION['user_id'] = $user->id;
+        $_SESSION['user_email'] = $user->email;
+        $_SESSION['user_name'] = $user->name;
+        redirect('pages/index');
+    }
+
+    public function logout()
+    {
+        unset($_SESSION['user_id']);
+        unset($_SESSION['user_email']);
+        unset($_SESSION['user_name']);
+        session_destroy();
+        redirect('users/login');
+    }
+
+    public function isLoggedIn()
+    {
+        if (isset($_SESSION['user_id'])) {
+            return tru;
+        } else {
+            return false;
         }
     }
 }
