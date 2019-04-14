9f26cf3ebc50de8e16ca3b427ba83884ba7e2c4d
Vit Safarik <vitsaf@gmail.com>
2019-04-14 17:43:50 +0200

    post model and display

--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -4,9 +4,21 @@
         <h1>Post</h1>
     </div>
     <div class="col-md-6">
-        <a href="<?php echo URLROOT;?>/posts/add" class="btn btn-primary pull-right">
+        <a href="<?php echo URLROOT;?>/posts/add" class="btn btn-primary pull-right mb-2">
         <i class="fas fa-pencil-alt mr-1"></i>Add Post
     </a>
     </div>
 </div>
+<?php foreach($data['posts'] as $post) : ?>
+
+    <div class="card card-body mb-3">
+        <h4 class="card-title"><?php echo $post->title; ?></h4>
+        <div class="bg-light p-2 mb-3">
+            written by <?php echo $post->name; ?> on <?php echo $post->postCreated; ?>
+        </div>
+        <p class="card-text"><?php echo $post->body; ?></p>
+        <a href="<?php echo URLROOT;?>/posts/show/<?php echo $post->postId; ?>" class="btn btn-dark">More</a>
+    </div>
+
+<?php endforeach; ?>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
